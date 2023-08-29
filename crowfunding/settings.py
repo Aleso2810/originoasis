99bcf2378ff1c5e2196f7367f19b77d460b62753
Alejandro Soria <pasm.28.10@gmail.com>
2023-08-29 12:14:22 -0500

    Pip

--- crowfunding/settings.py
+++ crowfunding/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') in [True,'True','true']
 
-ALLOWED_HOSTS = ['localhost','originoasis.azurewebsites.net','127.0.0.1:8000', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost','originoasis.azurewebsites.net','127.0.0.1:8000', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS=['htpps://originoasis.azurewebsites.net']
 
